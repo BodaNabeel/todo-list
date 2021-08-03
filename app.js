b6ffa20c697f1628e16a7e6b4ef0a4edfa0612c5
Nabeel Boda <NeedToExplore01@gmail.com>
2021-08-03 10:52:31 +0530

    empty field bug fixed

--- app.js
+++ app.js
@@ -16,53 +16,61 @@ let markup = function(info) {
     return structure
 }
 
+
+
 submitBtn.addEventListener('click', ()=> {
-    // 1. Gather the detail about task
-    let inputData = inputField.value
+    // 0. Checking input field 
+    if(inputField.value === "") {
+        alert(`task not mentioned`)
+    } else {
+// 1. Gather the detail about task
+let inputData = inputField.value
 
-    //  DOM skeleton 
-    let taskObjectForDOM = document.createElement("div");
-    taskObjectForDOM.classList.add("task")
-    let markup = `<p class="task__name">${inputData}</p>
-        <div class="task__btn">
-        <span class="material-icons task__icon icon__check"> echeck_box_outline_blank </span>
-        <span class="material-icons task__icon icon__edit"> edit </span>
-        <span class="material-icons task__icon icon__del"> delete </span>
-        </div>`
-    taskObjectForDOM.innerHTML = markup
+//  DOM skeleton 
+let taskObjectForDOM = document.createElement("div");
+taskObjectForDOM.classList.add("task")
+let markup = `<p class="task__name">${inputData}</p>
+    <div class="task__btn">
+    <span class="material-icons task__icon icon__check"> echeck_box_outline_blank </span>
+    <span class="material-icons task__icon icon__edit"> edit </span>
+    <span class="material-icons task__icon icon__del"> delete </span>
+    </div>`
+taskObjectForDOM.innerHTML = markup
 
-    // 3 Displaying task
-    taskContainer.appendChild(taskObjectForDOM)
+// 3 Displaying task
+taskContainer.appendChild(taskObjectForDOM)
 
-    // 4. Making input field blank
-    inputField.value = ""
+// 4. Making input field blank
+inputField.value = ""
 
-    // 5. Done/Check Icon function
-    const checkBtn = document.querySelectorAll(".icon__check") 
-    const taskName = document.querySelectorAll(".task__name")
-    checkBtn.forEach((el,i) => {
-        el.addEventListener("click", () => {
-        el.innerHTML = "check_box"
-        taskName[i].style.textDecoration = "line-through"
-        })
+// 5. Done/Check Icon function
+const checkBtn = document.querySelectorAll(".icon__check") 
+const taskName = document.querySelectorAll(".task__name")
+checkBtn.forEach((el,i) => {
+    el.addEventListener("click", () => {
+    el.innerHTML = "check_box"
+    taskName[i].style.textDecoration = "line-through"
     })
+})
 
-    // 6. Delete Icon Function
-    const delBtn = document.querySelectorAll(".icon__del")
-    const currentTask = document.querySelectorAll(".task")
-    delBtn.forEach((el,i) => {
-        el.addEventListener('click', () => {
-        currentTask[i].style.display = "none"
-        })
+// 6. Delete Icon Function
+const delBtn = document.querySelectorAll(".icon__del")
+const currentTask = document.querySelectorAll(".task")
+delBtn.forEach((el,i) => {
+    el.addEventListener('click', () => {
+    currentTask[i].style.display = "none"
     })
+})
 
-    // 7. Edit Icon Function
-    var editIcon = document.querySelectorAll(".icon__edit")
-    editIcon.forEach((el,i) => {
-        el.addEventListener('click', () => {
-            taskName[i].contentEditable = true;
-        })
+// 7. Edit Icon Function
+var editIcon = document.querySelectorAll(".icon__edit")
+editIcon.forEach((el,i) => {
+    el.addEventListener('click', () => {
+        taskName[i].contentEditable = true;
     })
+})
+    }
+    
 })
 
 
